3ae7bd6d88a4b1b361d30eaad8603918cdcc25f2
Daniel Shelest <daniel.shelest1@gmail.com>
2022-03-13 21:24:27 +0100

    Start Practise

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 "use strict";
 
-const answer = +prompt("Вам есть 18?", " 18");
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-console.log(typeof(answer) + answer);
+const personalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+      b = prompt("Насколько оцените его?", ""),
+      c = prompt("Один из последних просмотренных фильмов?", ""),
+      d = prompt("Насколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
